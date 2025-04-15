e02b93c53fbae8b90537d913e3d43245899098b6
Asfar Jamil <asfarj@withnubo.com>
2025-04-15 16:53:43 -0400

    style: small stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+  
+    onMount(() => {
+      console.log('Page component mounted');
+    });
+    </script>
+
+<div class="h-screen bg-slate-950">
+    <div class="flex flex-col h-full">
+        <h1 class="text-2xl text-center">good-afternoon</h1>
+    </div>
+</div>
